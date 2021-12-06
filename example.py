a9449262eef3b1759cbc5aab02108b864f4846c7
Strahinov Yordan Kostadinov <yordankostadinov.strahinov@extern.a1.at>
2021-12-06 10:10:25 +0200

    Initial commit

---
+++ example.py
@@ -0,0 +1,29 @@
+print("Hello world")
+
+val_int = 1
+val_float = 1.1
+val_bool = True
+
+
+print(val_int)
+print(val_bool)
+print(val_int+int(val_float))
+
+print(type(val_int))
+print(type(val_float))
+
+val_str = str("muhaha")
+
+print(val_str + " is a float")
+
+print("\"Hello, I'm [name], it's nice to meet you!\"")
+
+print("*******\n *****\n  ***\n   *")
+
+str_name = input("Tell me your name ? >>> ")
+str_surname = input("Tell me your surname ? >>> ")
+print(str_name + " " + str_surname)
+
+str_name = int(input("Tell me your name ? >>> "))
+str_surname = float(input("Tell me your surname ? >>> "))
+print(str_name + str_surname)
